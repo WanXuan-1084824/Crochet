793e013798093a3a71068339781bdbfcc5730d9f
WanXuan-1084824 <wanxuanlin2005@gmail.com>
2026-03-17 10:25:42 +0100

    startscherm html & css

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def startscherm():
+    return render_template('startscherm.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
